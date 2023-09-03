98116885046bff50dfe93cd373dd206d2796a2a8
vijaykgupta143 <vijaykgupta143@gmail.com>
2023-09-03 14:26:10 +0530

    home page and header ui

--- resources/views/apnadental/master.blade.php
+++ resources/views/apnadental/master.blade.php
@@ -26,7 +26,7 @@
 
 	<!-- YOUR CUSTOM CSS -->
 	<link href="{{ asset('public/assets/apnadental/css/custom.css') }}" rel="stylesheet">
-
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body>
 	<div class="layer"></div>
