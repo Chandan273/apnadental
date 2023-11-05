d8a3a04d38450b20cc570dfcb61b35b2d8a06696
chandan <rajansnuriya273@gmail.com>
2023-11-05 12:17:29 +0530

    added booking admin listings

--- resources/views/apnadental/master.blade.php
+++ resources/views/apnadental/master.blade.php
@@ -27,9 +27,9 @@
 
 	<!-- YOUR CUSTOM CSS -->
 	<link href="{{ asset('public/assets/apnadental/css/custom.css') }}" rel="stylesheet">
-	<link href="{{ asset('public/assets/css/custom.css') }}" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+	{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> --}}
+	<link href="{{ asset('public/assets/css/custom.css') }}" rel="stylesheet">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.5/dist/sweetalert2.all.min.js"></script>
 </head>
